ad57c2f872b33d72f26e2e6c9eeada7980fe82f5
Jason Yang <canyoncrestjy@gmail.com>
2021-06-24 15:42:41 -0700

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -15,4 +15,23 @@ function start() {
     )
 }
 
-start()
+video.addEventListener('play', () => {
+
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const size = { width: video.width, height: video.height }
+
+    faceapi.matchDimensions(canvas, size)
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks
+        ().withFaceexpressions()
+        const resizeddetections = faceapi.resizeResults(detections, size)
+        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
+        faceapi.draw.drawDetections(canvas, resizeddetections)
+        facepi.draw.drawFaceLandmarks(canvas, resizeddetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizeddetections)
+        
+    }, 100)
+})
